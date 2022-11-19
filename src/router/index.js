61beb0cd725140b3bc60e358365ce78964577136
lad <ladqmpw@163.com>
2022-11-19 17:57:33 +0800

    配置一级路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,28 @@ const router = createRouter({
     // 路由模式
     history: createWebHashHistory(),
     // 映射关系
-    routes: []
+    routes: [
+        {
+            path: "/",
+            redirect: "/home"
+        },
+        {
+            path: "/home",
+            component: () => import("@/views/home/Home.vue")
+        },
+        {
+            path: "/message",
+            component: () => import("@/views/message/Message.vue")
+        },
+        {
+            path: "/order",
+            component: () => import("@/views/order/Order.vue")
+        },
+        {
+            path: "/favor",
+            component: () => import("@/views/favor/Favor.vue")
+        }
+    ]
 })
 
 export default router
