adf0d0f8e123890caea7b3e43a8cff3890465b60
lad <ladqmpw@163.com>
2022-11-19 18:03:55 +0800

    设置一级路由链接，测试路由配置

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-  App.vue
+  <router-view />
+  <router-link to="/home">首页</router-link>
+  <router-link to="/favor">收藏</router-link>
+  <router-link to="/order">订单</router-link>
+  <router-link to="/message">信息</router-link>
 </template>
 
 <style scoped>
